7eb41076cea06c4175dad3f47d5a2f67d7516c85
Nathan Alex <nate745@gmail.com>
2019-07-08 02:10:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function happyHolidayTo(holiday, name){
 }
 
 function holidayCountdown(holiday, days){
-  return (`It's ${days} days until, ${holiday}!`)
+  return (`It's ${days} days until ${holiday}!`)
 
   holidayCountdown(`20`, `Mother's Day`)
 }
